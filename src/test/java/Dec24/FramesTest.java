f638eecbc5eb295722556d4bb02c6e4a90cfdc1c
Pavan Gottavada <gottavadapavan@gmail.com>
2025-12-24 22:52:56 +0530

    tasks added

---
+++ src/test/java/Dec24/FramesTest.java
@@ -0,0 +1,57 @@
+package Dec24;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FramesTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demoqa.com/frames");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    //=================================
+    // Frame 1 validation
+    //=================================
+    @Test
+    public void verifyFrame1() {
+
+        driver.switchTo().frame("frame1");
+
+        String text = driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertEquals(text, "This is a sample page");
+
+        driver.switchTo().defaultContent();
+    }
+
+    //=================================
+    // Frame 2 validation
+    //=================================
+    @Test
+    public void verifyFrame2() {
+
+        driver.switchTo().frame("frame2");
+
+        String text = driver.findElement(By.id("sampleHeading")).getText();
+        Assert.assertEquals(text, "This is a sample page");
+
+        driver.switchTo().defaultContent();
+    }
+}
